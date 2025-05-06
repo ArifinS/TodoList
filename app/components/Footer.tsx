e48c82356f0a63e4f9509cfc3f924b257ff6fe7a
Arifin opu <41758862+ArifinS@users.noreply.github.com>
2025-05-06 15:52:23 +0600

    Update files and add new UI zustant

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer: React.FC = () => {
       <footer className="py-6 md:py-8">
         <div className="container mx-auto">
           <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
-            aaaCopyright ©2024 | All rights reserved by Learn with Sumit  vaista amr 
+            aaaCopyright ©2024 | All rights reserved by Learn with Sumit khaisi tore
           </p>
         </div>
       </footer>
